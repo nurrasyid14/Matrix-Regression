c1952cf76fc1b547d1b3752852980c59a16deb65
nurrasyid14 <nurrasyid186@gmail.com>
2025-10-18 01:25:24 +0700

    __init__ call fix

--- app/regressor/__init__.py
+++ app/regressor/__init__.py
@@ -1,15 +1,15 @@
 # app/regressor/__init__.py
 
 from .linear_regression import LinearRegression
-from .ridge_regression import RidgeRegressor
-from .polynomial_regression import PolynomialRegressor
+from .ridge_regression import RidgeRegression
+from .polynomial_regression import PolynomialRegression
 from .metrics import r2_score, mean_squared_error
 from .preprocessing import MatrixPreprocessor
 
 __all__ = [
-    'LinearRegressor',
-    'RidgeRegressor',
-    'PolynomialRegressor',
+    'LinearRegression',
+    'RidgeRegression',
+    'PolynomialRegression',
     'r2_score',
     'mean_squared_error',
     'MatrixPreprocessor'
